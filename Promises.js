42a76da6a3e4a6f2261cd1e711b094573f2f5ae4
Divit Joshi <45086014+dj5353@users.noreply.github.com>
2019-11-10 22:40:47 +0530

    Add files via upload

---
+++ Promises.js
@@ -0,0 +1,24 @@
+// let basic = function(){
+//     return new Promise(function(resolve,reject) { 
+//        resolve("The javascript module is done") 
+//     })
+// };
+
+// let advance = function(mes){
+//     return new Promise(function(resolve,reject){
+//         resolve("The node.js is done")
+//     })
+// }
+
+// let expert = function(mes){
+//     return new Promise(function(resolve,reject){
+//         resolve("Congratulate you are a backend Developer")
+//     })
+// }
+// basic().then(function(mes){
+//     return (advance(mes))
+// }).then(function(mes){
+//     return (expert(mes))
+// }).then(function(){
+//     console.log("You are Elite");
+// })
